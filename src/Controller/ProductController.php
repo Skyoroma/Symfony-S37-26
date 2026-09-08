b16d23c9ba8306d55a032f49942f6cf9c3030ff1
Alexis-P-dev <alexis.paudoie@gmail.com>
2026-09-08 15:15:28 +0200

    Modif route productcontroller

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/products', name: 'app_products_')]
 final class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
+    #[Route('/list', name: 'list', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('product/index.html.twig', [
